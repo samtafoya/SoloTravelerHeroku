df287b7c8e48189b320268122764c1a3ec5e7e3b
samtafoya <sammit@live.com>
2020-04-10 15:08:39 -0700

    add api from aws

--- client/src/test.js
+++ client/src/test.js
@@ -5,7 +5,8 @@ class Test extends Component {
     constructor() {
         super();
         this.state = {
-            hello: 'hi'
+            hello: 'hi',
+            test: []
         };
     } 
 
@@ -13,15 +14,18 @@ class Test extends Component {
         // Get the passwords and store them in state
         fetch('http://ec2-54-183-192-99.us-west-1.compute.amazonaws.com:3000/api/allposts')
             .then(res => res.json())
-            .then(this.setState({ hello: res[0] }));
+            .then(test => this.setState({ test }));
     }
 
     render() {
+
+        const { test } = this.state;
+
         return (
             <div>
                 <h1>Test Page!</h1>
                 <button onClick={this.getHello}>call get hello</button>
-                {this.state.hello}
+                {test[0]}
             </div>
         );
     }
